ed6f83e7ccff6ec139ba4921181833703a6498aa
Danielle Callan <dcallan@upenn.edu>
2023-11-17 22:26:06 -0500

    htmlwidget

---
+++ R/bipartiteNetworkWidget.R
@@ -0,0 +1,31 @@
+library(htmlwidgets)
+library(dplyr)
+
+bipartiteNetwork <- function(data, width = NULL, height = NULL, elementId = NULL) {
+  if (!inherits(data, "data.frame") || !all(c("source", "target", "value") %in% names(data))) {
+    stop("Data must be a data frame with source, target, and value columns.")
+  }
+  unique_sources <- unique(data$source)
+  unique_targets <- unique(data$target)
+  edge_data <- data %>% 
+    filter(source %in% unique_sources & target %in% unique_targets) %>%
+    distinct(source, target, .keep_all = TRUE)
+
+  htmlwidgets::createWidget(
+    name = 'bipartitenetwork',
+    x = list(data = edge_data),
+    width = width,
+    height = height,
+    package = 'correlationMatrixAsNetwork',
+    elementId = elementId
+  )
+}
+
+bipartiteNetworkOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'bipartitenetwork', width, height, package = 'correlationMatrixAsNetwork')
+}
+
+renderBipartiteNetwork <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } 
+  htmlwidgets::shinyRenderWidget(expr, bipartiteNetworkOutput, env, quoted = TRUE)
+}
---
+++ inst/htmlwidgets/bipartitenetwork.js
@@ -0,0 +1,65 @@
+HTMLWidgets.widget({
+  name: 'bipartitenetwork',
+  type: 'output',
+
+  factory: function(el, width, height) {
+    var margin = { top: 20, right: 20, bottom: 30, left: 50 },
+        innerWidth = width - margin.left - margin.right,
+        innerHeight = height - margin.top - margin.bottom;
+
+    var svg = d3.select(el).append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    function renderValue(x) {
+      svg.selectAll('*').remove();
+
+      var xScale = d3.scaleOrdinal().domain(['source', 'target']).range([0, innerWidth]);
+      var yScale = d3.scalePoint().range([0, innerHeight])
+        .domain(d3.map(x.data, function(d){ return d.source; }).keys());
+
+      svg.selectAll('.node')
+        .data(x.data)
+        .enter()
+        .append('circle')
+        .attr('class', 'node')
+        .attr('r', 5)
+        .attr('cx', function(d) { return xScale('source'); })
+        .attr('cy', function(d) { return yScale(d.source); })
+        .enter()
+        .append('circle')
+        .attr('class', 'target')
+        .attr('r', 5)
+        .attr('cx', function(d) { return xScale('target'); })
+        .attr('cy', function(d, i) { return (i * (innerHeight / x.data.length)); })
+        .attr('fill', 'orange');
+
+      svg.selectAll('.link')
+        .data(x.data)
+        .enter()
+        .append('line')
+        .attr('class', 'link')
+        .attr('x1', function(d) { return xScale('source'); })
+        .attr('y1', function(d) { return yScale(d.source); })
+        .attr('x2', function(d) { return xScale('target'); })
+        .attr('y2', function(d, i) { return (i * (innerHeight / x.data.length)); })
+        .style('stroke', function(d) { return d.value > 0 ? 'green' : 'red'; })
+        .style('stroke-width', function(d) { return Math.abs(d.value) * 2; });
+    }
+
+    return {
+      renderValue: renderValue,
+
+      resize: function(newWidth, newHeight) {
+        svg.attr('width', newWidth).attr('height', newHeight);
+        innerWidth = newWidth - margin.left - margin.right;
+        innerHeight = newHeight - margin.top - margin.bottom;
+        xScale.range([0, innerWidth]);
+        yScale.range([0, innerHeight]);
+        renderValue(svg.datum());
+      }
+    };
+  }
+});
